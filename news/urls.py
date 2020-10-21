171ee1d7d14abf4e96cb2b9917e7b73f2f00d663
xyd18 <xyd18@mails.tsinghua.edu.cn>
2020-10-22 00:58:36 +0800

    搜索引擎，但是建立索引的时候报错ValueError: invalid literal for int() with base 10: 'sina_mobile'

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
+from rest_framework.routers import SimpleRouter
 from . import views
 
 urlpatterns = [
     path('index0',views.message,name='index0'),
 ]
+
+router = SimpleRouter()
+router.register(r'search', views.ArticleSearchViewSet, basename='search')
+urlpatterns += router.urls
--- news/views.py
+++ news/views.py
@@ -3,6 +3,11 @@ from django.http import JsonResponse, HttpResponse
 import json
 from .models import Articles,WeiboHot
 from django.core.exceptions import ValidationError
+from drf_haystack.viewsets import HaystackViewSet
+from .serializers import ArticleIndexSerializer
+from .paginations import ArticleSearchPageNumberPagination
+from haystack.views import SearchView as _SearchView
+
 
 def message(request):
     def gen_response(code: int, data: str):
@@ -28,3 +33,42 @@ def message(request):
                 for msg in WeiboHot.objects.all().order_by('-pk')[int(offset) : int(offset) + int(limit)]
             ])
         return response
+
+
+class ArticleSearchViewSet(HaystackViewSet):
+    """
+    文章搜索
+    """
+    index_models = [Articles]
+
+    serializer_class = ArticleIndexSerializer
+    pagination_class = ArticleSearchPageNumberPagination
+
+# class ArticleSearchViewSet(_SearchView):
+#     # 模版文件
+#     template = 'news/search.html'
+
+#     # 重写响应方式，如果请求参数q为空，返回模型News的热门新闻数据，否则根据参数q搜索相关数据
+#     def create_response(self):
+#         kw = self.request.GET.get('q', '')
+#         if not kw:
+#             # 如果没有索引值,就全部搜索出来
+#             show_all = True
+#             hot_news = models.HotNews.objects.select_related('news'). \
+#                 only('news__title', 'news__image_url', 'news__id'). \
+#                 filter(is_delete=False).order_by('priority', '-news__clicks')
+
+#             paginator = Paginator(hot_news, settings.HAYSTACK_SEARCH_RESULTS_PER_PAGE)
+#             try:
+#                 page = paginator.page(int(self.request.GET.get('page', 1)))
+#             except PageNotAnInteger:
+#                 # 如果参数page的数据类型不是整型，则返回第一页数据
+#                 page = paginator.page(1)
+#             except EmptyPage:
+#                 # 用户访问的页数大于实际页数，则返回最后一页的数据
+#                 page = paginator.page(paginator.num_pages)
+#             return render(self.request, self.template, locals())
+#         else:
+#             show_all = False
+#             qs = super(SearchView, self).create_response()
+#             return qs
