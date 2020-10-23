7089bc2f6e1c43dceb37bc396dcc7c192e0fd52e
王子涵 <zh-wang17@mails.tsinghua.edu.cn>
2020-10-23 13:54:45 +0800

    热点演进-查找关键词返回新闻列表

--- news/inverted_index/search.py
+++ news/inverted_index/search.py
@@ -2,7 +2,6 @@ import json
 import datetime
 import time
 import re,jieba
-
 import sys,numpy
 sys.path.append("/home/ubuntu/xxswl-backend/")
 import os,django
@@ -11,31 +10,35 @@ django.setup()
 
 from news.models import Articles
 
+f=open("inverted_index_cluster.json",'r',encoding="utf-8")
+f1=open("inverted_index_article.json",'r',encoding="utf-8")
+inverted_index_cluster=json.load(f)
+inverted_index_article=json.load(f1)
+
 def to_timestamp(strtime:str): ###将字符串时间"2020-05-03 12:01:54"转成时间戳
     timearray = time.strptime(strtime, '%Y-%m-%d %H:%M:%S')
     timestamp = int(time.mktime(timearray))
     return timestamp
-f=open("inverted_index_cluster.json",'r',encoding="utf-8")
-inverted_index=json.load(f)
 
-
-def search(keyword: str, starttime,endtime):  #热点演进
-    ans_list=[]
-    dic=inverted_index[keyword]
-    L = sorted(dic.items(), key=lambda item: item[1], reverse=True)
-    cluster_id=L[0][0]
-    sina_rollnews = Articles.objects.filter(cluster_id=cluster_id).order_by("time")
+def search(keyword: str, starttime,endtime):
+    #热点演进: 输入关键词，返回和这个关键词有关的新闻列表
+    ans_list = []
+    dic_cluster = inverted_index_cluster[keyword]
+    dic_article = inverted_index_article[keyword]
+    print(dic_article)
+    L = sorted(dic_cluster.items(), key = lambda item: item[1], reverse = True)
+    cluster_id = L[0][0]  ###找到最相关的一个聚类
+    sina_rollnews = Articles.objects.filter(cluster_id = cluster_id).order_by("time")
     for article in sina_rollnews:
-        text = article.title+article.body
-        text = re.sub(r"[^\u4e00-\u9fa5]", "", text)
-        seg_word = list(jieba.cut(text))
-        ttime=article.time
-        if(keyword in seg_word and starttime<to_timestamp(ttime) and to_timestamp(ttime)<endtime):
-            print(article.title,article.time)
-            ans_list.append({"title":article.title,"time":article.time,"body":article.body})
+        if str(article.id) not in dic_article.keys() or dic_article[str(article.id)] <= 3:  #查倒排索引，找到含有三个以上这个关键词的文档
+            continue
+        ttime = article.time
+        if(starttime<to_timestamp(ttime) and to_timestamp(ttime)<endtime):  #时间比对
+            print(article.id,article.cluster_id,article.title, article.time)
+            ans_list.append({"title":article.title, "time":to_timestamp(article.time), "body":article.body})
     return ans_list
 
-anslist=search("美股",1602690735,1602863535)
+anslist=search("疫苗",0,16028635350000)
 print(anslist)
 
 def wordcloud(cluster_id,topk):
@@ -62,5 +65,3 @@ def wordcloud(cluster_id,topk):
     for l in L:
         dic[l[0].encode("utf-8").decode("utf-8")] = l[1]
     print(dic)
-wordcloud(2,30)
-print(inverted_index["进行"])
