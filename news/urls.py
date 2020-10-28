d6ca77732c8a8ddcc237f5fa902b555a4b33ff89
xyd18 <xyd18@mails.tsinghua.edu.cn>
2020-10-28 14:46:41 +0800

    BUGIFIX

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('index0',views.message,name='index0'),
     path('wordcloud/<cluster_id>/<topk>',views.GetWordcloud,name='wordcloud'),
-    path('timeline/<keyword>/<starttime>/<endtime>',views.GetTimeline,name='timeline')
+    path('timeline/<keyword>/<starttime>/<endtime>',views.GetTimeline,name='timeline'),
     path('search/',views.search, name='search')
 ]
