fa1489e9bc538b1e85e1a67a9519c08dc9f8da65
xyd18 <xyd18@mails.tsinghua.edu.cn>
2020-11-10 10:42:44 +0800

    #11 添加搜索相关单元测试

--- news/tests.py
+++ news/tests.py
@@ -23,6 +23,23 @@ class TestNews(TestCase):
             response = requests.get('http://62.234.121.168:30000/news/search/' + para)
             return response
 
+        def get_search_heatmap(q=None, time_from=None, time_to=None):
+            para_list = []
+            if q is not None:
+                para_list.append("q=" + q)
+            if time_from is not None:
+                para_list.append("from=" + time_from)
+            if time_to is not None:
+                para_list.append("to=" + time_to)
+
+            if para_list.__len__ == 0:
+                para = ""
+            else:
+                para = ("?" + "&".join(para_list))
+
+            response = requests.get('http://62.234.121.168:30000/news/search_heatmap/' + para)
+            return response
+
         def get_search_date_cluster_info(q=None):
             para_list = []
             if q is not None:
@@ -38,4 +55,28 @@ class TestNews(TestCase):
 
         # test case 1 with default args
         response = get_search_news()
+        self.assertEqual(response.status_code, 200)
+        response = get_search_heatmap()
+        self.assertEqual(response.status_code, 200)
+        response = get_search_date_cluster_info()
+        self.assertEqual(response.status_code, 200)
+
+        # test case 2 with args
+        response = get_search_news('a')
+        self.assertEqual(response.status_code, 200)
+        response = get_search_news('a', '2020-10-1')
+        self.assertEqual(response.status_code, 200)
+        response = get_search_news('a', '2020-10-1', '2020-10-25')
+        self.assertEqual(response.status_code, 200)
+        response = get_search_news('a', '2020-10-1', '2020-10-25', 10)
+        # test case 2 with args
+        self.assertEqual(response.status_code, 200)
+        response = get_search_heatmap('a')
+        self.assertEqual(response.status_code, 200)
+        response = get_search_heatmap('a', '2020-10-1')
+        self.assertEqual(response.status_code, 200)
+        response = get_search_heatmap('a', '2020-10-1', '2020-10-25')
+        self.assertEqual(response.status_code, 200)
+
+        response = get_search_date_cluster_info('a')
         self.assertEqual(response.status_code, 200)
