cb38d3179ffad017ba687abb6eacf05af720b806
2017011415 <zh-wang17@mails.tsinghua.edu.cn>
2020-11-19 09:53:43 +0800

    更新数据库

--- news/similarity/similarity.py
+++ news/similarity/similarity.py
@@ -24,7 +24,6 @@ def Jaccard(grams_A, grams_B):#Jaccard相似度
 
 def Find_n_sim(id): ####找到和编号为id的文章最接近的n个新闻
     origin_news=Articles.objects.filter(id=id)[0]
-
     title = re.sub(r"[^\u4e00-\u9fa5]", "", origin_news.body)
     keywords=origin_news.keywords.split("@@@")[0]
     cluster_id=origin_news.cluster_id
@@ -42,7 +41,6 @@ def Find_n_sim(id): ####找到和编号为id的文章最接近的n个新闻
         sim_dic[id]=Jaccard(gram_A,gram_B)
     L = sorted(sim_dic.items(), key=lambda item: item[1], reverse=True)
     sim_ans=""
-    print(L)
     for i in range(min(30,len(L))):
         sim_ans+=str(L[i][0])+","
         news=Articles.objects.filter(id=L[i][0])
@@ -51,6 +49,7 @@ def Find_n_sim(id): ####找到和编号为id的文章最接近的n个新闻
     origin_news.save()
 
 articles=Articles.objects.filter()
+print("begin......")
 for article in articles:
     id=article.id
     print(id)
